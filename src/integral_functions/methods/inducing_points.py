e1c885f8bece107f36da130ed67c942e38d904b3
Alex Johnson <abjohnson117@gmail.com>
2024-10-09 17:59:17 -0700

    Creating repository

---
+++ src/integral_functions/methods/inducing_points.py
@@ -0,0 +1,52 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+def midpoint_to_disc(discpoint: int | float, gridpoint: int | float) -> int | float:
+    r"""Given a discretization point $x_i$ and a grid point $g_i$, this function calculates the following point via the formula
+    \[
+        x_{i+1} = 2g_i - x_i.
+    \]
+    The use case of this formulation applies to integration via midpoint quadrature rules. In this way, the grid point is considered
+    to be the midpoint as it falls exactly in the middle of the discretization points $x_i$ and $x_{i+1}$.
+
+    Args:
+        discpoint (int | float): The preceding discretization point used to calculate the following point.
+        gridpoint (int | float): The midpoint used for calculating the next discretization point.
+
+    Returns:
+        int | float: The next discretization point in the sequence.
+    """
+    # assert gridpoint > discpoint
+    return 2 * gridpoint - discpoint
+
+
+def get_discretizations(
+    L: int | float, U: int | float, grid_points: NDArray, grid_to_disc: callable
+) -> NDArray:
+    r"""Creates appropriate discretizations from grid points. Effectively, this function will generate points such that
+    every pair of adjacent elements in the returned vector will have a grid point (from grid_points) between it.
+    The method of point generation depends on the callable grid_to_disc.
+
+    Args:
+        L (int | float): Lower bound on the age range of interest.
+        U (int | float): Upper bound on the age range of interest.
+        grid_points (NDArray): The n x 1 vector of points that serve as the evaluation points of the function we seek to model.
+                The evaluation is done at inference time in solving for \theta. We use the grid points to build age bins.
+        grid_to_disc (callable): Function to map a grid point and the immediately prior discretization point into a discretization point.
+
+    Returns:
+        NDArray: The (n+1) x 1 vector of discretization points along which we create our age bins.
+    """
+    grid_points = np.asarray(grid_points)
+    grid_len = grid_points.shape[0]
+
+    disc_list = [grid_to_disc(L, grid_points[0])]
+    for i in range(1, grid_len):
+        discpoint = disc_list[i - 1]
+        gridpoint = grid_points[i]
+        disc_list.append(grid_to_disc(discpoint, gridpoint))
+    disc_list.insert(0, L)
+    assert disc_list[-1] == U
+
+    return np.array(disc_list)
---
+++ src/integral_functions/weight_functions.py
@@ -0,0 +1,39 @@
+import numpy as np
+from numpy.typing import NDArray
+
+from integral_functions.methods import get_discretizations
+
+
+def get_weights(
+    L: int | float, U: int | float, population_density: NDArray, grid_points: NDArray
+) -> NDArray:
+    r"""Function that accepts a valid range of ages [L,U], a vector of population densities, and a vector of midpoints
+    and generates the weights used in a numerical integration via midpoint quadrature rules. Importantly, this function
+    does not return the numerically integrated values, only the weights; i.e., standard numerical integrators solve integration by
+    \[
+        \int_a^b f(x)\mathrm{d}x \approx \sum_{i=0}^n w_i f(x_i)
+    \]
+    and this function merely returns the weights (w_1, \dots, w_n). In mortality estimation, the weights are comprised as follows:
+    \[
+        w_i = p_i(discretizations_{i+1} - discretizations_{i})
+    \]
+    where $p_i$ is the $i$th element of the population_density vector, and the discretizations vector is obtained from the get_discretizations
+    function, which builds age bins around the grid_points argument.
+
+    Args:
+        L (int | float): Lower bound on the age range of interest.
+        U (int | float): Upper bound on the age range of interest.
+        population_density (NDArray): The n x 1 vector of population densities per age bin.
+                In mortality estimation, this may be viewed as the proportion dead of the total population per age bin.
+        grid_points (NDArray): The n x 1 vector of points that serve as the evaluation points of the function we seek to model.
+                In other words, the grid_points are the collection of points (x_1, \dots, x_n) in the equation above.
+                The evaluation is done at inference time in solving for \theta. We use the grid points to build age bins.
+
+    Returns:
+        NDArray: A vector of the weights $w_i$ for $i=1,\dots, n$.
+    """
+    discretizations = get_discretizations(L, U, grid_points)
+    age_bin_lengths = np.diff(discretizations)
+    weights = population_density * age_bin_lengths
+
+    return weights
