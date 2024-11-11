d5b98aa7d702820cea8115d01bd75731291eeefc
Alex Johnson <abjohnson117@gmail.com>
2024-11-11 14:45:32 -0800

    Addign some simulation code

--- src/integral_functions/simulation/age_intervals.py
+++ src/integral_functions/simulation/age_intervals.py
@@ -53,3 +53,24 @@ def bounded_intervals_generator(
     age_ranges = np.vstack([age_starts, age_ends])
 
     return age_ranges
+
+
+def int_bounded_intervals_generator(
+    age_start: Numeric,
+    age_end: Numeric,
+    num_groups: Numeric,
+    age_interval_lb: Numeric = 0,
+    age_interval_ub: Numeric = 5,
+) -> NDArray:
+    age_starts = np.random.randint(
+        low=age_start,
+        high=age_end - age_interval_ub,
+        size=num_groups,
+    )
+    age_intervals = np.random.randint(
+        low=age_interval_lb, high=age_interval_ub, size=num_groups
+    )
+    age_ends = age_starts + age_intervals
+    age_ranges = np.vstack([age_starts, age_ends])
+
+    return age_ranges
